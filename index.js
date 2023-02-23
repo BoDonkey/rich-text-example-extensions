a617f7bda8900f63db1d32ac130330c46f03ce6c
Bob Means (BoDonkey) <robert@apostrophecms.com>
2023-02-23 17:33:21 -0500

    fix missing comma

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = {
   bundle: {
     directory: 'modules',
     modules: [ `@apostrophecms/typography`, '@apostrophecms/smilies' ]
-  }
+  },
   init(self) {
     console.log('👋 from the rich text widget extension!');
   }
