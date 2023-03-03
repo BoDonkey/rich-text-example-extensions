3987bac6da522fd37873540930905a60aa969e3a
Bob Means (BoDonkey) <robert@apostrophecms.com>
2023-03-03 17:16:04 -0500

    correct config

--- modules/@apostrophecms/typography/ui/apos/tiptap-extensions/typography.js
+++ modules/@apostrophecms/typography/ui/apos/tiptap-extensions/typography.js
@@ -1,5 +1,5 @@
 import Typography from '@tiptap/extension-typography';
 export default (options) => {
-  const configuration = self.apos.modules['@apostrophecms/typography'].ttTypoConfig || {};
+  const configuration = self.apos.modules['@apostrophecms/rich-text-widget'].ttTypoConfig || {};
   return Typography.configure(configuration);
 };
