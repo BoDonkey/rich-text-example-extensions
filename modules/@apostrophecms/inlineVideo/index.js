abac08699b63dfd01a30a5e895458e596c6e1ce1
Bob Means (BoDonkey) <robert@apostrophecms.com>
2023-03-12 09:49:00 -0400

    start refactor

--- modules/@apostrophecms/inlineVideo/index.js
+++ modules/@apostrophecms/inlineVideo/index.js
@@ -5,28 +5,31 @@ module.exports = {
   },
   init(self) {
     self.enableBrowserData();
+    self.toolbarToAllowedTags();
+    (self.options.editorTools = {
+      ...self.options.editorTools,
+      inlineVideo: {
+        component: 'TiptapInlineVideo',
+        // TODO: l18n
+        label: 'Inline Video',
+        command: 'setInlineVideo'
+      }
+    }),
+      (self.options.tiptapTypes = {
+        ...self.options.tiptapTypes,
+        inlineVideo: ['iframe', 'div']
+      });
   },
   extendMethods(self) {
     return {
       getBrowserData(_super, req) {
         const initialData = _super(req);
-        const finalTools = {
-          ...initialData.tools,
-          inlineVideo: {
-            component: 'TiptapInlineVideo',
-            // TODO: l18n
-            label: 'Inline Video',
-            icon: 'play-box-icon',
-            command: 'setInlineVideo'
-          }
-        };
         const finalData = {
           ...initialData,
-          tools: finalTools,
           ttVideoConfig: self.options.inlineVideoConfig
-        }
+        };
         return finalData;
       }
-    }
+    };
   }
 };
